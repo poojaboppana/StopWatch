ad13a68e3b1ec6173abe12e25a5733f121d90de0
pooja <poojaboppana99@gmail.com>
2025-06-07 00:38:18 +0530

    StopWatch

---
+++ StopWatch.js
@@ -0,0 +1,8 @@
+let time=0;
+function stopwatch(){
+time++;
+let millisec= time%100;
+let sec= Math.floor((time/100)%60);
+let min= Math.floor((time/(100*60)));
+document.getElementById("res").value=`${min}:${sec}:${millisec}`;
+} setInterval(stopwatch,10);
